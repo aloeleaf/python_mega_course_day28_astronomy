9a211760ecd9833c346a42607d92d0416b9ec56e
aloeleaf <szabolcs.sandor@gmail.com>
2023-06-16 23:22:52 +0200

    finished project

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import streamlit as st
 import requests
 
-st.set_page_config(layout="wide")
+# init web page
+st.set_page_config(layout="centered")
 
 # set parameters for request nasa api
 api = "mqqWKKUXLL9fWVyUHNokL4ujxvFkMpwWOiQtckif"
 url = "https://api.nasa.gov/planetary/apod?"  \
       f"api_key={api}"
 
+# get source from api
 response = requests.get(url)
+# convert json form
 content = response.json()
-img = response.content["url"]
-
-with open("image.jpg", "+rb") as img:
+# get image url
+img = requests.get(content["url"]).content
+# create image
+with open("image.jpg", "+wb") as file:
     file.write(img)
 
 
-
-#st.header("Nasa pictures")
-#st.image(content["url"])
+# create web page
+st.header(content["title"])
+st.image("image.jpg")
 st.write(content["explanation"])
 
 
